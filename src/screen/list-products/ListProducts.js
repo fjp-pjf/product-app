7ac470488a6ed2d26fa2af5bedd965c25a089c75
fjp_pjf <feminjustin07@gmail.com>
2022-06-11 11:06:46 +0530

    list products view

---
+++ src/screen/list-products/ListProducts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ListItem from "../../components/list-item/ListItem";
+import "./list-products.css";
+
+const ListProducts = () => {
+  return (
+    <div>
+      <ListItem />
+    </div>
+  );
+};
+
+export default ListProducts;

30d19f23aaa4f823919d11ecadb6051456e96cf3
fjp_pjf <feminjustin07@gmail.com>
2022-06-11 11:07:04 +0530

    landing screen view

---
+++ src/screen/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopNavbar from "../../components/navbar/TopNavbar";
+import ListProducts from "../list-products/ListProducts";
+
+const Home = () => {
+  return (
+    <>
+      <TopNavbar />
+      <ListProducts />
+    </>
+  );
+};
+
+export default Home;
